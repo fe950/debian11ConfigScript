d8757c74a72c3cf28131c7613a58a093941469fb
fe950 <70313832+fe950@users.noreply.github.com>
2022-02-13 20:06:18 +0100

    First

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# debian11ConfigScript
- Configurationscript for Debian 11 post install- Lightveight VM with desktop
+#  Configuration script for Debian 11 to create a minimalistic machine ready configured with Xfce desktop.
+Suited for Vm or Desktop use.
+
+
+
+### Options 
+- update_system
+- configures norwegian time and date format
+- remove_grub_delay from 5 to 1 sec.
+- install_Xfce (xfdesktop4 xfce4-settings xfce4-panel xfwm4, xfce4-session)
+- install optional Xfce tools (Mousepad Gigolo garcon xfce4-datetime-plugin)
+- install_firewall (ufw  gufw)  (need manual activation incase ssh,start with "enable ufw")
+- install_basic debian apps (curl man-db manpages dnsutils mc lynx nmap baobab nethogs ark okular gwenview git xeyes)
+- install common desktop tools (gimp, vlc gparted, ffmpeg, wireshark, geogebra, librecad, obs studio)
+- installs qbittorrent
+- configuration of terminal (aliases and nano as preselected editor)
+ -install minimalistic browser (qutebrowser and python3)
+- install python (python3 and pip)
+- install_firefox (firefox-esr with optional extension-ublock-origin and configuration)
+-Snap
+- Spotify (installed with snap
+- installs Mullvad Vpn client (direct download from Mullvad)
+- removes software (only xeyes)
+- reboot
+
+
+### HowTo
+ 1. Download Debian 11 netinstall https://www.debian.org/download 
+ 2. Install Debian without any packages or window managers.
+ 3. Login, and fix the issue that debian dont support sudo out of the box. (replace joe with username)
+```
+su -
+usermod -aG sudo joe
+```
+4.install wget and download script and give it permission to run
+ ```
+su
+apt install wget -y && wget https://raw.githubusercontent.com/fe950/debian11/main/easy_debian11.sh && chmod +x easy_debian11.sh
+usermod -aG sudo YOUR_USERNAME
+exit
+```
+5. Review and make changes to fit your needs.
+```
+cat script.sh
+```
+6. Run script
+```
+./easy_debian11.sh
+
+```
+
+
+### TODO
+- Fix sudo 
+- Change Dns server to 1.1.1.1
+- Automatic innstallation av pub og private ssh keys
+- Cryptomator or simular
+- rsync or simular client to Onedrive, google Drive
+- Configure Sambashare
+- Nvidia driver installation
+- Debian Hardening
+- flatpack
+- tor browser
---
+++ config_debian11_vm_xfce.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+bold=`echo -en "\e[1m"`
+purple=`echo -en "\e[35m"`
+orange=`echo -en "\e[33m"`
+red=`echo -en "\e[31m"`
+green=`echo -en "\e[32m"`
+lightblue=`echo -en "\e[94m"`
+underline=`echo -en "\e[4m"`
+normal=`echo -en "\e[0m"`
+#if root stop
+if ! [ "$EUID" -ne 0 ] ; then
+    echo "${orange}Dont run as root.$normal"
+    exit
+fi
+#install sudo if not already installed, NOT WORKING
+if ! hash sudo 2>/dev/null; then
+    echo "${purple}installing sudo$normal"
+    su -c "apt install sudo ; echo '$USER ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo"
+fi
+
+
+Answer_Yes_to_Everything_and_run_Unattended() {
+    update_system
+    remove_grub_delaychange_locales
+    install_xfce_core
+    install_xfce_tools
+    install_debian_basics_apps
+    install_common_desktop_apps
+    configure_terminal
+    run_nethogs_as_root
+    install_python3
+    install_minimal_browser
+    install_firefox
+    install_spotify
+    install_snap
+    install_mullvad
+    remove_apps
+    reboot    
+}
+
+update_system() {
+    sudo apt -qq update && sudo apt -qq upgrade -y && sudo apt -qq full-upgrade -y && sudo apt -qq autoremove -y
+}
+remove_grub_delay() {
+    sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/' /etc/default/grub
+    sudo update-grub
+}
+change_locales() {
+    echo "nb_NO.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen > /dev/null
+    sudo locale-gen
+    sudo update-locale LC_TIME=nb_NO.UTF-8
+}
+# minimal install guide https://www.devuan.org/os/documentation/dev1fanboy/en/minimal-xfce-install.html
+install_xfce_core() {
+    sudo apt -qq install xfdesktop4 xfce4-settings xfce4-panel xfwm4 xfce4-session xfce4-terminal thunar -y
+}
+install_xfce_tools() {
+    sudo apt -qq install Mousepad Gigolo garcon xfce4-datetime-plugin -y   
+}
+install_firewall() {
+    sleep 1
+    echo $bold$orange'Firewall is not enabled, remember to start with "enable ufw"'$normal
+    sleep 1
+    echo "Install will continue in a few seconds"
+    sleep 3
+    sudo apt -qq install ufw gufw -y
+    #sudo ufw enable #enable this if you dont install over ssh
+}
+install_debian_basics_apps() {
+    sudo apt -qq install curl man-db manpages dnsutils net-tools tcpdump mc lynx nmap nethogs viewnior git tree -y
+}
+install_common_desktop_apps() {
+    sudo apt -qq install gimp vlc gparted ffmpeg wireshark geogebra librecad obs-studio -y
+}
+configure_terminal() {
+    #root terminal
+    echo "alias tid='date "+%T"'" | sudo tee -a /root/.bashrc > /dev/null
+    echo "alias klocka='date "+%T"'" | sudo tee -a /root/.bashrc > /dev/null
+    echo "alias klockan='date "+%T"'" | sudo tee -a /root/.bashrc > /dev/null
+    echo "alias ll='ls -alF'" | sudo tee -a /root/.bashrc > /dev/null
+    echo "alias la='ls -A'" | sudo tee -a /root/.bashrc > /dev/null
+    echo "alias l='ls -CF'" | sudo tee -a /root/.bashrc > /dev/null
+    echo 'SELECTED_EDITOR="/bin/nano"' | sudo tee /root/.selected_editor > /dev/null
+    #user terminal
+    echo "alias tid='date "+%T"'" >> ~/.bashrc
+    echo "alias klocka='date "+%T"'" >> ~/.bashrc
+    echo "alias klockan='date "+%T"'" >> ~/.bashrc
+    echo "alias ll='ls -alF'" >> ~/.bashrc
+    echo "alias la='ls -A'" >> ~/.bashrc
+    echo "alias l='ls -CF'" >> ~/.bashrc
+    echo 'SELECTED_EDITOR="/bin/nano"' >> ~/.selected_editor
+}  
+run_nethogs_as_root() {
+    #Allow to run nethogs as not root, Debian consider this unsecure, use at your own risk.
+    echo sudo chmod u+s /sbin/nethogs
+    echo "alias nethogs='/sbin/nethogs'" >> ~/.bashrc
+}
+install_python3() {
+    sudo apt -qq install python3 pythin3-pip -y
+}
+install_minimal_browser() {
+    echo ${red} "This will also install Python3"
+    sudo apt -qq install qutebrowser -y
+}
+install_firefox() {
+    sudo apt -qq install firefox-esr -y
+    firefox-esr&
+    sleep 5
+    pkill -f firefox
+    sleep 2
+    install_firefox_plugins
+    configure_firefox
+ }   
+install_firefox_plugins() {
+    sudo apt -qq  webext-ublock-origin-firefox  -y
+}    
+configure_firefox() { 
+    prefix="/home/$USER/.mozilla/firefox"
+    suffix=".default-esr/"
+    path=""
+    for f in "${prefix}"/*"${suffix}"; do
+        path="$f""user.js"
+    done
+    echo $path
+    echo "user_pref(\"browser.contentblocking.category\",custom);
+    user_pref(\"browser.privatebrowsing.autostart\", true);
+    user_pref(\"browser.shell.checkDefaultBrowser\", true);
+    user_pref(\"network.cookie.cookieBehavior\",1);
+    user_pref(\"network.cookie.lifetimePolicy\",2);
+    user_pref(\"privacy.donottrackheader.enabled\", true);
+    user_pref(\"privacy.trackingprotection.enabled\", true);
+    user_pref(\"privacy.trackingprotection.socialtracking.enabled\", true);
+    user_pref(\"signon.rememberSignons\", false);
+    " | sudo tee $path
+}
+install_spotify() {
+    sudo apt -qq install snapd -y
+    sudo snap install core
+    sudo snap install spotify
+}
+install_snap() {
+    sudo apt -qq install snapd -y
+    sudo snap install core
+}
+install_mullvad() {
+    wget --content-disposition https://mullvad.net/download/app/deb/latest
+    sudo apt install -y ./MullvadVPN*.deb
+    sleep 1
+    rm ./MullvadVPN*.deb
+}
+remove_apps() {
+    sudo apt -qq remove tree -y && sudo apt autoremove -y
+}
+reboot() { 
+    sudo shutdown -r now
+}
+
+functions_array=(
+    Answer_Yes_to_Everything_and_run_Unattended
+    update_system
+    remove_grub_delaychange_locales
+    install_xfce_core
+    install_xfce_tools
+    install_debian_basics_apps
+    install_common_desktop_apps
+    configure_terminal
+    run_nethogs_as_root
+    install_python3
+    install_minimal_browser
+    install_firefox
+    install_spotify
+    install_snap
+    install_mullvad
+    remove_apps
+    reboot
+    )
+if [[ " $# " -ne 0 ]]; then
+    for i in $@
+    do
+        if [[ " ${functions_array[@]} " =~ " ${i} " ]]; then
+            echo "${green}Option ${i} valid$normal"
+            ${i} #exec function
+        else
+            echo "${red}Unknown option ${i}$normal"
+        fi
+    done
+else
+    for i in "${functions_array[@]}"; do
+        formatted1=${i//_/ }
+        formatted2=${formatted1^}
+        echo -n "${purple}${formatted2^} (y/n)? $normal"
+        read answer
+        if [ "$answer" != "${answer#[Yy]}" ] ;then
+            ${i}
+        fi
+    done
+fi
