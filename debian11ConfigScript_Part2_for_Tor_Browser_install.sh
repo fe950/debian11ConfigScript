7ef13212320b77bde4c51614f25ef2e829f37099
fe950 <70313832+fe950@users.noreply.github.com>
2022-03-30 01:55:21 +0200

    Create debian11ConfigScript_Part2_for_Tor_Browser_install.sh
    Part 2, only needed when installing Tor browser

---
+++ debian11ConfigScript_Part2_for_Tor_Browser_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+# This is only neccesary to tun if Tor Browser was installed
+# You also have another good packagemaneger isntalled now as an requirement.
+# Check out https://flatpak.org/
+sudo flatpak run com.github.micahflee.torbrowser-launcher
